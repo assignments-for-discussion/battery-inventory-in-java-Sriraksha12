b61962d17b1736976eb42704acb3ef90129014fe
Sriraksha P <129095717+Sriraksha12@users.noreply.github.com>
2023-09-08 20:32:40 +0530

    Update Main.java
    Calculated the Soh%, added conditions to check SOh.
    Added additional test cases.

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -11,8 +11,9 @@ public class Main {
     CountsBySoH counts = new CountsBySoH();
     
     for(int capacity : presentCapacities){
-      double sohPercentage = (capacity * 100.0) / 120.0;
-
+      double sohPercentage = (capacity * 100.0) / 120.0; // Assuming rated capacity is 120 Ah
+      
+      //Conditions to check SoH of batteries
       if(sohPercentage > 80){
         counts.healthy++;
       }
@@ -28,12 +29,42 @@ public class Main {
 
   static void testBucketingByHealth() {
     System.out.println("Counting batteries by SoH...\n");
+
+    // Given test case
     int[] presentCapacities = {115, 118, 80, 95, 91, 77};
     CountsBySoH counts = countBatteriesByHealth(presentCapacities);
-    
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
+
+    
+    // test with empty array
+    int[] capacities2 = {};
+    CountsBySoH counts2 = countBatteriesByHealth(capacities2);
+    assert(counts2.healthy == 0);
+    assert(counts2.exchange == 0);
+    assert(counts2.failed == 0);
+
+   // test with all the batteries healthy
+    int[] capacities3 = {120,119,120,118,120};
+    CountsBySoH counts3 = countBatteriesByHealth(capacities2);
+    assert(counts3.healthy == 5);
+    assert(counts3.exchange == 0);
+    assert(counts3.failed == 0);
+
+// test with all the batteries need exchange
+    int[] capacities4 = {64,60,62,65,63};
+    CountsBySoH counts4 = countBatteriesByHealth(capacities2);
+    assert(counts4.healthy == 0);
+    assert(counts4.exchange == 5);
+    assert(counts4.failed == 0);
+
+// test with all the batteries failed
+    int[] capacities5 = {30,40,45,55,58};
+    CountsBySoH counts5 = countBatteriesByHealth(capacities2);
+    assert(counts5.healthy == 0);
+    assert(counts5.exchange == 0);
+    assert(counts5.failed == 5);
     
     System.out.println("Done counting :)\n");
   }
